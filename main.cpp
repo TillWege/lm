55988f696a68e1bf89fe1e53711fc7b555a44f25
TillWege <tillwege@gmail.com>
2025-02-26 00:08:04 +0100

    implement some things

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 #include "cfgpath.h"
 #include "json.hpp"
 
-using json = nlohmann::json;
+#define VERSION "0.1.0"
 
+using json = nlohmann::json;
 
 void printArgs(int argc, char** argv)
 {
@@ -13,12 +14,6 @@ void printArgs(int argc, char** argv)
 	{
 		std::cout << argv[i] << std::endl;
 	}
-}
-
-
-bool matchArg(std::string cliArg, std::string Arg)
-{
-	return false;
 }
 
 std::string getConfigFilePath()
@@ -46,35 +41,105 @@ void on_receive_response(const ollama::response& response)
 	};
 }
 
+void printModels()
+{
+	auto m = ollama::list_models();
+	for (auto& model : m)
+	{
+		std::cout << model << std::endl;
+	}
+
+}
+
+void printVersion()
+{
+	std::cout << "lm version " << VERSION << std::endl;
+}
+
+struct arg
+{
+	std::string argName;
+	std::function<void()> callback;
+	std::string description;
+};
+
+std::vector<arg> args;
+
+void printHelp()
+{
+	std::cout << "lm - a command line interface for llms" << std::endl;
+	std::cout << "Usage: lm [options]" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Options:" << std::endl;
+	for (auto& arg : args)
+	{
+		std::cout << "  --" << arg.argName << ", " << arg.description << std::endl;
+	}
+}
+
+bool matchArg(const std::string& cliArg, const arg& Arg)
+{
+	if (cliArg == "--" + Arg.argName)
+	{
+		return true;
+	}
+
+	if (cliArg == std::string("-") + Arg.argName.at(0))
+	{
+		return true;
+	}
+
+	return false;
+}
+
+
 int main(int argc, char** argv)
 {
+	args.push_back({
+		"help",
+		printHelp,
+		"Print this help message"
+	});
+	args.push_back({
+		"models",
+		printModels,
+		"List available models"
+	});
+	args.push_back({
+		"version",
+		printVersion,
+		"Print version"
+	});
+
+
 	if (argc < 2)
 	{
 		std::cout << "starting chat mode" << std::endl;
 	}
 	else
 	{
-		std::string arg = std::string(argv[1]);
-		if (arg.starts_with("--"))
+		for (auto& arg : args)
 		{
-			std::cout << "got cli arg " << arg << std::endl;
-		}
-		else
-		{
-			std::cout << "generating response" << std::endl;
-			std::string prompt;
-			for (int i = 2; i < argc; i++)
+			if(matchArg(std::string(argv[1]), arg))
 			{
-				prompt += argv[i];
-				if (i < argc - 1)
-				{
-					prompt += " ";
-				}
+				arg.callback();
+				return 0;
 			}
-
-			std::function<void(const ollama::response&)> response_callback = on_receive_response;
-			ollama::generate("llama3.2:3b-instruct-Q5_K_M", prompt, on_receive_response);
 		}
+
+		std::cout << "generating response" << std::endl;
+		std::string prompt;
+		for (int i = 2; i < argc; i++)
+		{
+			prompt += argv[i];
+			if (i < argc - 1)
+			{
+				prompt += " ";
+			}
+		}
+
+		std::function<void(const ollama::response&)> response_callback = on_receive_response;
+		ollama::generate("llama3.2:3b-instruct-Q5_K_M", prompt, on_receive_response);
 	}
 
 	return 0;
